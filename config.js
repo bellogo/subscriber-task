d52d768482979dd0b821aeae9637b805a4647593
Bela <ufuomaogodo@gmail.com>
2022-11-19 14:25:10 +0100

    initialize sequelize

--- config.js
+++ config.js
@@ -3,5 +3,8 @@ require('dotenv').config({ silent: true, path: '.env' });
 
 module.exports = {
   port: process.env.PORT,
-  
+  dbName: process.env.DBNAME,
+  dbUser: process.env.DBUSER,
+  dbPassword: process.env.DBPASSWORD,
+  dbHost: process.env.DBHOST,
 };
---
+++ src/database/config/config.js
@@ -0,0 +1,28 @@
+const config = require('../../../config');
+
+module.exports = {
+  development: {
+    username: config.dbUser,
+    password: config.dbPassword,
+    database: config.dbName,
+    host: config.dbHost,
+    dialect: 'postgres',
+    logging: false,
+  },
+  // test: {
+  //   username: config.dbUser,
+  //   password: config.dbPassword,
+  //   database: config.dbName,
+  //   host: config.dbHost,
+  //   dialect: 'postgres',
+  //   logging: false,
+  // },
+  // production: {
+  //   username: config.dbUser,
+  //   password: config.dbPassword,
+  //   database: config.dbName,
+  //   host: config.dbHost,
+  //   dialect: 'postgres',
+  //   logging: false,
+  // },
+};
